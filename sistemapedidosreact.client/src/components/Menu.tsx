230833d4adaaf303eb047b3dcc1aa22bcc2ddad7
maximiliano_hermosilla <maximiliano_hermosilla@hotmail.com>
2025-09-13 03:11:07 -0300

    Carousel manual con css y ref scrollable

--- sistemapedidosreact.client/src/components/Menu.tsx
+++ sistemapedidosreact.client/src/components/Menu.tsx
@@ -1,11 +1,15 @@
 import "./Menu.css";
 import "../App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import GetLastMenu from "../services/menu.service";
 import CardProduct from "./CardProduct";
+import { BsArrowLeftCircleFill } from "react-icons/bs";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+
 
 export default function Menu() {
     const [menu, setMenu] = useState<any>();
+    const scrollContainerRef = useRef(null);
 
     useEffect(() => {
         getLastMenu();
@@ -19,15 +23,31 @@ export default function Menu() {
         setMenu(data);
     }
 
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            (scrollContainerRef!.current! as HTMLDivElement).scrollLeft -= 200; // Adjust scroll amount as needed
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            (scrollContainerRef!.current! as HTMLDivElement).scrollLeft += 200; // Adjust scroll amount as needed
+        }
+    };
+
     const contents = menu === undefined ? <p className="text-lg font-semibold text-center text-cyan-700 w-full my-3">No hay productos para mostrar</p> :
         <> {menu?.items?.map((item: any) => <CardProduct key={item.id} product={item}></CardProduct>)} </>;
 
     return (
-        <>
+        <section className="menu__carrousel">
             <h2 className="w-full text-center text-2xl font-bold text-gray-600 my-3">Menu</h2>
-            <section className="horizontal__scrol__wrapper menu__section flex-column md:flex gap-5 overflow-x-auto mb-3">
-                {contents}
-            </section>
-        </>
+            <div className="menu__container">
+                <BsArrowLeftCircleFill className="arrow arrow-left" onClick={scrollLeft}></BsArrowLeftCircleFill>
+                <div ref={scrollContainerRef} className="horizontal__scrol__wrapper menu__section flex-column md:flex gap-5 overflow-x-auto mb-3">
+                    {contents}
+                </div>
+                <BsArrowRightCircleFill className="arrow arrow-right" onClick={scrollRight}></BsArrowRightCircleFill>
+            </div>
+        </section>
     )
 }
